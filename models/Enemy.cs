d52667e7fe6baca15a502b8d38a3ca019db4210f
Yoon Lopez <yoon.lopez.7e9@itb.cat>
2026-04-10 10:01:50 +0200

    Added Enemy

---
+++ models/Enemy.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace pp7_heroengine_oop_quest_yoon_lpz.models
+{
+    public abstract class Enemy
+    {
+        private int _health, _maxHealth, _strength;
+
+        protected int Health {
+            get => _health;
+            set { if (Tools.isNaturalNumber(value)) {
+                    _health = value;
+                    MaxHealth = value;
+                }
+            }
+        }
+
+        protected int MaxHealth
+        {
+            get => _maxHealth;
+            set { if (value >= Health) _maxHealth = value; }
+        }
+
+        protected int Strength
+        {
+            get => _strength;
+            set { if (Tools.isNaturalNumber(value)) _strength = value; }
+        }
+
+        protected Enemy(int health, int strength) {
+            Health = health;
+            MaxHealth = health;
+            Strength = strength;
+        }
+    }
+}
